c444166ac15982a29dbae85371f1814cd3c4d0f8
Iris Cheng <chengiris66@gmail.com>
2018-12-05 08:55:59 -0800

    Added profile

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -3,19 +3,55 @@ var textProfile = `A former professional electrical/software/field appliation en
 Appreciated by the industry clients and colleagues for her technical troubleshooting and managerial skills, Iris has demonstrated her ability to multitask enigneer duties with business devlopment.<br><br>
 
 Now Iris is looking for a career change to become an IT professional to fullfills her childhood dream.`
-$(".card-text").html(textProfile);
+
 
 var previousClickedTab = "profile";
 
 $(document).on("click", ".nav-item", function (event) {
-    var li = $(event.currentTarget).attr("data-name").trim();
-    console.log(li);
-    console.log(event);
+    var activeTab = $(event.currentTarget).attr("data-name").trim();
+    console.log(activeTab);
+    // console.log(event);
 
     switch (previousClickedTab) {
-        case["profile"]: $("#profile").removeClass("active");
+        case "profile":
+            $("#profile").removeClass("active");
             break;
-
+        case "current":
+            $("#current").removeClass("active");
+            break;
+        case "past":
+            $("#past").removeClass("active");
+            break;
+        case "education":
+            $("#education").removeClass("active");
+            break;
+        case "contact":
+            $("#contact").removeClass("active");
+            break;
     };
 
+    switch (activeTab) {
+        case "profile":
+            $(".card-text").html(textProfile);
+            $("#profile").addClass("active");
+            break;
+        case "current":
+            $(".card-text").html("");
+            $("#current").addClass("active");
+            break;
+        case "past":
+            $(".card-text").html("");
+            $("#past").addClass("active");
+            break;
+        case "education":
+            $(".card-text").html("");
+            $("#education").addClass("active");
+            break;
+        case "contact":
+            $(".card-text").html("");
+            $("#contact").addClass("active");
+            break;
+        default:
+    };
+    previousClickedTab = activeTab;
 });
