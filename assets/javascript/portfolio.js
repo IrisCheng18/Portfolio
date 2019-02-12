9e947be30658f2f70b2ad397508e4615d7bf481b
Iris Cheng <chengiris66@gmail.com>
2019-02-12 01:06:52 -0800

    updates

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,11 +1,16 @@
-var textProfile = `A former professional electrical/software/field appliation engineer and project manager with a strong operational background, Iris has been involved in the system integration development as well as project management in the display/auto industry for over 15 years.<br><br> 
+// var txtProfile = `A former professional electrical/software/field appliation engineer and project manager with a strong operational background, Iris has been involved in the system integration development as well as project management in the display/auto industry for over 15 years.<br><br> 
 
-Appreciated by the industry clients and colleagues for her technical troubleshooting and managerial skills, Iris has demonstrated her ability to multitask enigneer duties with business devlopment.<br><br>
+// Appreciated by the industry clients and colleagues for her technical troubleshooting and managerial skills, Iris has demonstrated her ability to multitask enigneer duties with business devlopment.<br><br>
 
-Now Iris is looking for a career change to become an IT professional to fullfill her childhood dream.`
+// Now Iris is looking for a career change to become an IT professional to fullfill her childhood dream.`
 
-let textContact = `<br>Address: San Diego, CA <br><br>Email: yue_cheng@hotmail.com<br><br>Phone: 858-221-3080`
-
+let txtContact = `<br>Email: chengiris66@gmail.com<br><br>Phone: 858-221-3080`;
+let txtEduction = `<br>2019 Certification of Full-Stack Web Development<br>UCSD Extension, San Diego, CA<br>
+                    <br>2005 Master of Science (Computer Vision and Image Processing)<br>Southern Illinois University Edwardsville, Edwardsville, IL<br>
+                    <br>1996 Bachelor of Enginnering (Electrical Engineering)<br>Beijing University of Aeronautics and Astronautics, Beijing, China`;
+let txtPast =`<br>2001-2016 Westar Display Technologies, Inc
+              <br>~2013-2016 Field Support and Marketing in Asia, Beijing, China
+              <br>~2001-2013 Electrical/Software Engineer, St. Charles, MO, U.S.A.`;
 let previousClickedTab = "current";
 
 $(document).on("click", ".nav-item", function (event) {
@@ -16,12 +21,12 @@ $(document).on("click", ".nav-item", function (event) {
     // console.log(event);
 
     switch (previousClickedTab) {
-        case "profile":
-            $("#profile").removeClass("active");
-            break;
+        // case "profile":
+        //     $("#profile").removeClass("active");
+        //     break;
         case "current":
             $("#current").removeClass("active");
-            $(".current-item").attr("style", "width: 20rem; height: 25rem; display: none;");
+            $(".current-item").attr("style", "width: 20rem; height: 26rem; display: none;");
             break;
         case "past":
             $("#past").removeClass("active");
@@ -35,25 +40,25 @@ $(document).on("click", ".nav-item", function (event) {
     };
 
     switch (activeTab) {
-        case "profile":
-            $("#content").html(textProfile);
-            $("#profile").addClass("active");
-            break;
+        // case "profile":
+        //     $("#content").html(txtProfile);
+        //     $("#profile").addClass("active");
+        //     break;
         case "current":
             $("#content").html("");
-            $(".current-item").attr("style", "width: 20rem; height: 25rem;");
+            $(".current-item").attr("style", "width: 20rem; height: 26rem;");
             $("#current").addClass("active");
             break;
         case "past":
-            $("#content").html("");
+            $("#content").html(txtPast);
             $("#past").addClass("active");
             break;
         case "education":
-            $("#content").html("");
+            $("#content").html(txtEduction);
             $("#education").addClass("active");
             break;
         case "contact":
-            $("#content").html(textContact);
+            $("#content").html(txtContact);
             $("#contact").addClass("active");
             break;
         default:
